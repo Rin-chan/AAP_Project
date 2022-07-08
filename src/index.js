3d37fe66e2b2654d26c63cb28bbe44e1875c1a5b
Rin <203803F@mymail.nyp.edu.sg>
2022-07-08 11:30:03 +0800

    Database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import Navigator from './navigations';
 
-const Index = () => <Navigator />;
+// THIS IS LEFT HERE FOR DEMOSTRATION ON HOW TO IMPORT DATABASE INTO YOUR PAGES
+/*
+import databaseValue  from '../database.json';
+
+const { CosmosClient } = require("@azure/cosmos");
+
+const endpoint = databaseValue.endpoint;
+const key = databaseValue.key;
+const client = new CosmosClient({ endpoint, key });
+
+async function main() {
+    const { database } = await client.databases.createIfNotExists({ id: "ToDoList" });
+    const { container } = await database.containers.createIfNotExists({ id: "Items" });
+}
+
+main().catch((error) => {
+  console.error(error);
+});
+*/
+
+
+const Index = () => {
+    return (
+        <Navigator />
+    );
+}
 
 export default Index;
